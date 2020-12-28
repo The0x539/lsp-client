4923527af1a40b27bc54ec68ba99b5120abf3300
The0x539 <the0x539@gmail.com>
2020-12-27 22:04:22 -0600

    async, for reasons

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,11 @@
 use crate::error::{Error, ProtocolViolation, Result};
 use crate::msg::{Notification, OutboundMessage, Request, Response};
 
-use std::{
-    ffi::OsStr,
-    io::{BufRead, BufReader, Read, Write},
-    process::{Child, ChildStdin, ChildStdout, Command, Stdio},
+use std::{ffi::OsStr, process::Stdio};
+
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    process::{Child, ChildStdin, ChildStdout, Command},
 };
 
 use lsp_types::notification::Notification as NotificationTrait;
@@ -53,15 +54,15 @@ impl Client {
         Ok(lines.join("\r\n"))
     }
 
-    fn send_msg(&mut self, msg: &[u8]) -> std::io::Result<()> {
-        self.stdin.write_all(msg)
+    async fn send_msg(&mut self, msg: &[u8]) -> std::io::Result<()> {
+        self.stdin.write_all(msg).await
     }
 
-    pub fn recv_msg(&mut self) -> std::io::Result<Vec<u8>> {
+    pub async fn recv_msg(&mut self) -> std::io::Result<Vec<u8>> {
         let mut content_len = None;
         let mut line = String::new();
         loop {
-            self.stdout.read_line(&mut line)?;
+            self.stdout.read_line(&mut line).await?;
             if line == "\r\n" {
                 break;
             } else if let Some(len) = line.strip_prefix("Content-Length: ") {
@@ -71,18 +72,20 @@ impl Client {
             line.clear();
         }
         let mut buf = vec![0; content_len.expect("no len")];
-        self.stdout.read_exact(&mut buf)?;
+        self.stdout.read_exact(&mut buf).await?;
         Ok(buf)
     }
 
-    pub fn request<R: RequestTrait>(&mut self, params: R::Params) -> Result<R::Result> {
+    pub async fn request<R: RequestTrait>(&mut self, params: R::Params) -> Result<R::Result> {
         let id = self.req_id();
         let content = Request::<R> { id, params };
 
         let msg = Self::build_msg(content).unwrap();
-        self.send_msg(msg.as_bytes()).map_err(Error::SendMsg)?;
+        self.send_msg(msg.as_bytes())
+            .await
+            .map_err(Error::SendMsg)?;
 
-        let r_msg = self.recv_msg().map_err(Error::RecvMsg)?;
+        let r_msg = self.recv_msg().await.map_err(Error::RecvMsg)?;
         let response: Response<R::Result> = serde_json::from_slice(&r_msg).expect("bad data");
 
         assert_eq!(response.id, id);
@@ -94,14 +97,16 @@ impl Client {
         }
     }
 
-    pub fn notify<N: NotificationTrait>(&mut self, params: N::Params) -> Result<()> {
+    pub async fn notify<N: NotificationTrait>(&mut self, params: N::Params) -> Result<()> {
         let content = Notification::<N> { params };
         let msg = Self::build_msg(content).unwrap();
-        self.send_msg(msg.as_bytes()).map_err(Error::SendMsg)?;
+        self.send_msg(msg.as_bytes())
+            .await
+            .map_err(Error::SendMsg)?;
         Ok(())
     }
 
-    pub fn initialize(
+    pub async fn initialize(
         &mut self,
         cwd: &str,
         capabilities: ClientCapabilities,
@@ -125,8 +130,8 @@ impl Client {
             locale: None,
         };
 
-        let res = self.request::<Initialize>(params)?;
-        self.notify::<Initialized>(InitializedParams {})?;
+        let res = self.request::<Initialize>(params).await?;
+        self.notify::<Initialized>(InitializedParams {}).await?;
         Ok(res)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use lsp_client::capabilities::caps;
 use lsp_client::client::Client;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
     let mut client = Client::new("rust-analyzer")?;
 
     let cwd = std::env::current_dir()?.canonicalize()?;
-    let res = client.initialize(cwd.to_str().unwrap(), caps()).unwrap();
+    let res = client
+        .initialize(cwd.to_str().unwrap(), caps())
+        .await
+        .unwrap();
     let _x = format!("{}", serde_json::to_string_pretty(&res)?);
     println!("got here");
 
     loop {
-        let msg = client.recv_msg()?;
+        let msg = client.recv_msg().await?;
         println!("{}", std::str::from_utf8(&msg).unwrap());
     }
 }
